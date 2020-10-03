8f36113acd5eb37e1173db3121e4cdca795213d6
Benjamin Walker <benjaminwalker222@gmail.com>
2020-10-03 18:12:06 +0100

    add project input

--- src/domcontroller.js
+++ src/domcontroller.js
@@ -2,9 +2,42 @@ import {AppController} from "./appcontroller";
 
 let app = new AppController();
 let container = document.querySelector("#content");
+let formContainer = document.querySelector("#form-container");
+let newProjectButton = document.querySelector("#newproject");
+newProjectButton.addEventListener("click", () => {
+    formContainer.appendChild(DOMController.createProjectInputForm());
+})
 
 export class DOMController {
-    static createProjectCard(title) {
+    static renderProjects() {
+        container.innerHTML = "";
+        app.projects.forEach((project, index) => {
+            container.appendChild(this.createProjectCard(project.title))
+        });
+    }
+
+    static createProjectInputForm() {
+        let inputForm = document.createElement("div");
+        inputForm.classList.add("input-form");
+
+        let projectTitle = document.createElement("h2");
+        projectTitle.textContent = "Project Title:"
+
+        let projectTitleInput = document.createElement("input");
+
+        let addProjectButton = document.createElement("button");
+        addProjectButton.textContent = "Add Project";
+        addProjectButton.addEventListener("click", () => {
+            app.addProject(projectTitleInput.value);
+            this.renderProjects()
+            formContainer.innerHTML = "";
+        })
+
+        inputForm.append(projectTitle, projectTitleInput, addProjectButton);
+        return inputForm;
+    }
+
+    static createProjectCard(title, index) {
         let projectCard = document.createElement("div");
         projectCard.classList.add("project-card");
 
@@ -16,6 +49,10 @@ export class DOMController {
 
         let removeButton = document.createElement("button");
         removeButton.textContent = "Remove project";
+        removeButton.addEventListener("click", () => {
+            app.removeProject(index);
+            this.renderProjects();
+        })
 
         let todoContainer = document.createElement("div");
         todoContainer.classList.add("todo-container");
@@ -27,6 +64,26 @@ export class DOMController {
         projectCard.append(projectHeader, todoContainer, newTodoButton);
 
         return projectCard;
+    }
+
+    static createTodoCard(title, description, dueDate, numTasksRemaining) {
+        let todoCard = document.createElement("div");
+        todoCard.classList.add("todo-card");
+
+        let todoTitle = document.createElement("h3");
+        todoTitle.textContent = title;
+
+        let todoDescription = document.createElement("p");
+        todoDescription.textContent = description;
+
+        let todoDate = document.createElement("p");
+        todoDate.textContent = dueDate;
+
+        let tasksRemaining = document.createElement("p");
+        tasksRemaining.textContent = `${numTasksRemaining} tasks remaining`;
+
+        todoCard.append(todoTitle, todoDescription, todoDate, tasksRemaining);
+        return todoCard;
     }
 
     static addCard(title) {
